f43f4ecda38f0d39e5f34a12d82f3c7c84f4e4b9
Nevin Gilbert <nevin.gil@live.com>
2019-10-26 21:03:50 -0400

    Add caps for speaker volume

--- app/src/main/java/com/example/soundtouchproject/VolumeThread.java
+++ app/src/main/java/com/example/soundtouchproject/VolumeThread.java
@@ -54,9 +54,9 @@ public class VolumeThread implements Runnable {
                 Log.println(Log.DEBUG, "Feature", "No impl.");
             }
 
-            if (targetIntenstiy - micIntensity > ERROR_INTENSITY) {
+            if (targetIntenstiy - micIntensity > ERROR_INTENSITY && currentSpeakerVolume <= 100) {
                 currentSpeakerVolume += VOLUME_CHANGE;
-            } else if (micIntensity - targetIntenstiy < ERROR_INTENSITY) {
+            } else if (micIntensity - targetIntenstiy < ERROR_INTENSITY && currentSpeakerVolume >= 0) {
                 currentSpeakerVolume += VOLUME_CHANGE;
             }
 
